416b5be6b020a789926e09a426bf592744be1825
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-10 17:16:58 -0300

    Update sobre.js

--- pages/sobre.js
+++ pages/sobre.js
@@ -1,7 +1,19 @@
 import React from "react";
+import Link from "next/link";
 
 const Sobre = () => {
-  return <h1>Sobre</h1>
+  return (
+  <div>
+    <h1>Sobre</h1>
+    <div>
+      <Link href='/'>
+        <a>Home</a>
+      </Link>
+    </div>
+  
+  </div>
+  
+  )
 }
 
 export default Sobre
