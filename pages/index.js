2c0ba3eecd33181ba0c70de3ad7ced17e8331498
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-10 17:13:40 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Index =() => {
-  return <h1>Olá Semana FullStack Master</h1>
+  return (
+    <div>
+      <h1>Olá Semana FullStack Master</h1>
+        <div>
+          <Link href='/sobre'>
+            <a>Sobre</a>
+          </Link>
+        </div>
+    </div>
+  )
 }
 
 export default Index
