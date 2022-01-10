3264516184d9efd75923d25786c4bf3148f1caae
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-10 17:43:42 -0300

    Create pesquisa.js

---
+++ pages/pesquisa.js
@@ -0,0 +1,23 @@
+import next from "next";
+import Link from "next/link";
+
+const Pesquisa = () => {
+  return (
+    <div>
+      <h1>Pesquisa</h1>
+      <div>
+        <Link href='/'>
+          <a>Home</a>
+        </Link>
+        <Link href='/sobre'>
+          <a>Sobre</a>
+        </Link>
+        <Link href='/contato'>
+          <a>Contato</a>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Pesquisa
