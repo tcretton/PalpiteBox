51047622c849942df37bcb7fdcd7d5a0e909237e
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-12 22:07:35 -0300

    changes

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,7 +4,7 @@ const Header =  () => {
   return ( 
   <div className='bg-gray-200 p-4'>
     <div className='container mx-auto'>
-      PalpiteBox
+      <img src='/logo_palpitebox.png' alt='PalpiteBox'></img>
     </div>
   </div>
   )
