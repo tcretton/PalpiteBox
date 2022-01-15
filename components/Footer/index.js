bbb1c9f48e71dcbda12a7edc3a0a6e99dfa2da65
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-14 21:34:51 -0300

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,13 +3,14 @@ import Link from "next/link";
 
 const Footer = () => {
 return (
-<div className='bg-gray-700'>
+<div className='bg-gray-700 p-4'>
   <div className='container mx-auto text-center font-semibold text-gray-100'>
-    Projeto desenvolvido por:
-    <a href='https://cretton.info'>Tércio Cretton / </a> 
-    <a href='https://linkedin.com/in/tercio-cretton'>Linkedin / </a>
-    <a href='https://github.com/tcretton'>/ Github</a>
-    <a href='https://devpleno.com'> Durante o curso FullStack Master - Devpleno.com</a>
+    Projeto desenvolvido por:{' '}
+    <a className='hover:underline' href='https://cretton.info'>Tércio Cretton</a> {' '}
+    <a className='hover:underline' href='https://linkedin.com/in/tercio-cretton'>Linkedin</a>{' '}
+    <a className='hover:underline' href='https://github.com/tcretton'>Github</a>
+    </br>
+    <a className='hover:underline' href='https://devpleno.com'> No curso FullStack Master - Devpleno.com</a>
   </div>
 </div>
 )
