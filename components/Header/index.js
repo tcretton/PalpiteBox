e7f33c027e746363111553d18223fe892bb3c3d2
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-15 10:40:38 -0300

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,16 +9,20 @@ const Header =  () => {
         <img className='mx-auto' src='/logo_palpitebox.png' alt='PalpiteBox'></img>
       </div>
     </div>
-    <div className='bg-gray-300 p-4 shadow-lg text-center'>
+    <div className='bg-gray-300 p-2 shadow-lg text-center'>
       <Link href='/sobre'>
-        <a className='px-2 hover:underline'>Sobre</a>
+        <a className='px-1 hover:underline'>Sobre</a>
       </Link>
       <Link href='/contato'>
-        <a className='px-2 hover:underline'>Contato</a>
+        <a className='px-1 hover:underline'>Contato</a>
       </Link>
       <Link href='/pesquisa'>
-        <a className='px-2 hover:underline'>Pesquisa</a>
+        <a className='px-1 hover:underline'>Pesquisa</a>
       </Link>
+    </div>
+    <div>
+      
+      
     </div>
   </React.Fragment>
   )
