39c15a56b6d0e2991218a162efe83a9e5da6522d
Tércio Cretton <32882410+tcretton@users.noreply.github.com>
2022-01-22 18:57:02 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,15 @@ import Link from 'next/link'
 const Index =() => {
   return (
     <div>
-      <h1>Home - Olá Semana FullStack Master</h1>
-        <div>
-          ffff
-        </div>
+      <p className='mt-6 text-center'>
+        O restaurante X sempre busca atender melhor seus clientes.<br/>
+        Sua opnião é muito importante para nós.
+      </p>
+      <div>
+        <Link href='/pesquisa'>
+          <a>Dar opnião ou sugestão</a>
+        </Link>
+      </div>
     </div>
   )
 }
